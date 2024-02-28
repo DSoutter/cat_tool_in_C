2f3d5a871f1d5d16db8558c1bb8a196562df6b66
DSoutter <duncan.soutter@gmail.com>
2024-02-28 20:14:42 +0000

    Added some testing comments

--- myCat.c
+++ myCat.c
@@ -11,3 +11,9 @@ int main()
         write (STDOUT_FILENO, buffer, bytes_read);
     };
 }
+// running gcc myCat.c creates the a.out executable
+// run using ./a.out <test.txt
+// the '<' is for input redirection
+// ./a.out <test.txt <test2.txt does both files sequentially
+// ./a.out >output.txt
+// the '>' is for output redirection so instead of outputting to the terminal, it outputs to the file
