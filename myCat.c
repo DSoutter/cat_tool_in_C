e249de4dd1b493d664c45c5d43e61f8046da3eff
DSoutter <duncan.soutter@gmail.com>
2024-02-28 20:31:13 +0000

    Takes input file and reads out

--- myCat.c
+++ myCat.c
@@ -4,12 +4,19 @@
 #define BUFFER_SIZE 80
 char buffer[BUFFER_SIZE];
 
-int main()
+int main(int argc, char *argv[])
 {
+    int fd = STDIN_FILENO; // file descriptor
+
+    if (argc > 1) {
+        fd = open(argv[1], O_RDONLY);
+    }
     ssize_t bytes_read;
-    while ((bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE))) {
+    while ((bytes_read = read(fd, buffer, BUFFER_SIZE))) {
         write (STDOUT_FILENO, buffer, bytes_read);
-    };
+    }
+
+    close(fd);
 }
 // running gcc myCat.c creates the a.out executable
 // run using ./a.out <test.txt
@@ -17,3 +24,7 @@ int main()
 // ./a.out <test.txt <test2.txt does both files sequentially
 // ./a.out >output.txt
 // the '>' is for output redirection so instead of outputting to the terminal, it outputs to the file
+// Adding fd and fd = open("./test.txt", O_RDONLY); reads directly from the file
+// argc = count
+// *argv = pointer to array of strings
+// argv[0] = name of program so argv[1] is first input parameter
