57d95dc261ff8b58e46be853255f0b97aab64ea6
DSoutter <duncan.soutter@gmail.com>
2024-02-28 20:33:24 +0000

    Error handling

--- myCat.c
+++ myCat.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 #define BUFFER_SIZE 80
 char buffer[BUFFER_SIZE];
@@ -10,6 +11,10 @@ int main(int argc, char *argv[])
 
     if (argc > 1) {
         fd = open(argv[1], O_RDONLY);
+        if (fd == -1){ // function returns -1 if error opening the file
+            write(STDERR_FILENO, "no file\n", 8);
+            exit(EXIT_FAILURE);
+        }
     }
     ssize_t bytes_read;
     while ((bytes_read = read(fd, buffer, BUFFER_SIZE))) {
